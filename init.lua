3fc7a91a646cc0019159652a2c690eb3d1e2cb43
horak <jaroslav.horak@jiffaco.cz>
2015-04-19 23:53:34 +0200

    Prvni verze se spoustou chyb
    Neumi se znova pripojit na wifi
    Obcas nacte blbosti, asi nekontroluje crc
    A ani nahodou nema moznost data zalohovat do souboru a poslat se spozdenim

---
+++ init.lua
@@ -0,0 +1,28 @@
+--init.lua
+print("Setting up WIFI...")
+wifi.setmode(wifi.STATION)
+--modify according your wireless router settings
+wifi.sta.config("JIFFACO","***REMOVED***855")
+wifi.sta.connect()
+tmr.alarm(1, 1000, 1, function() 
+if wifi.sta.getip()== nil then 
+print("IP unavaiable, Waiting...") 
+else 
+tmr.stop(1)
+print("Config done, IP is "..wifi.sta.getip())
+dofile("send2cloud.lua")
+end 
+end)
+-- Pozhasinani ledek
+-- cervena 
+gpio.mode(8, gpio.OUTPUT)
+gpio.write(8, gpio.LOW)
+-- zelena
+gpio.mode(6, gpio.OUTPUT)
+gpio.write(6, gpio.LOW)
+-- modra
+gpio.mode(7, gpio.OUTPUT)
+gpio.write(7, gpio.LOW)
+-- mala cervena zhruba uprostred prosvecovala
+gpio.mode(2, gpio.OUTPUT)
+gpio.write(2, gpio.HIGH)
