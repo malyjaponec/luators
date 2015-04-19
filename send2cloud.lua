9b3ed91ea809cf4af8b55f31cb63e8fe794e6cbf
horak <jaroslav.horak@jiffaco.cz>
2015-04-19 23:53:34 +0200

    Prvni verze se spoustou chyb
    Neumi se znova pripojit na wifi
    Obcas nacte blbosti, asi nekontroluje crc
    A ani nahodou nema moznost data zalohovat do souboru a poslat se spozdenim

---
+++ send2cloud.lua
@@ -0,0 +1,53 @@
+PIN = 4 --  data pin, GPIO2
+
+function getTemp()
+  Humidity = 0
+  Temperature = 0
+
+  dht22 = require("dht22")
+  dht22.read(PIN)
+  Temperature = dht22.getTemperature()
+  Humidity = dht22.getHumidity()
+
+  if Humidity == nil then
+    return 0
+  end
+  
+  Temperature = Temperature / 10
+  Humidity = Humidity / 10
+
+  print ("Temperature: "..Temperature)
+  print ("Humidity: "..Humidity)
+  return 1
+end
+
+function sendData()
+  if 1 == getTemp() then
+    -- conection to thingspeak.com
+    print("Sending data to thingspeak.com")
+    conn=net.createConnection(net.TCP, 0) 
+    conn:on("receive", function(conn, payload) print(payload) end)
+    -- api.thingspeak.com 184.106.153.149
+    conn:connect(80,'184.106.153.149') 
+    conn:send("GET /update?key=6XJ1AWU739JA0J9G&field1="..Temperature.."&field2="..Humidity.." HTTP/1.1\r\n") 
+    conn:send("Host: api.thingspeak.com\r\n") 
+    conn:send("Accept: */*\r\n") 
+    conn:send("User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n")
+    conn:send("\r\n")
+    conn:on("sent", function(conn)
+    print("Closing connection")
+    conn:close()
+    end)
+    conn:on("disconnection", function(conn)
+    print("Got disconnection...")
+    end)
+  else
+    print("Chyba cteni dat")
+  end
+
+end
+
+-- send data every X ms to thing speak
+tmr.alarm(2, 60000, 1, function() sendData() end )
+-- a rovnou to zmer
+sendData()
