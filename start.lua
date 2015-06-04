eaf313d49e62d16bd9baab0923d6d9730b0a42a4
Jaroslav Horak <horak@jablocom.com>
2015-06-04 11:50:48 +0200

    Funkcni kod, stabilne prenasi pokud je vse prelozeno
    Vyhozena knihovna aptools.lua, vyhledani AP je napsane primo do start.lua,
    soubor aptools.lua je tedy zbytecny
    
    test.lua a testy.lua jsou pouze pokusy, nic nedelaji, projektu se neucastni.
    
    Vylepseno mereni baterie, ukazovalo i o neco vic nez realita, ale neni
    odskousene kolik to bude ukazovat ted, zmena konstanty bulharske.

--- start.lua
+++ start.lua
@@ -1,32 +1,144 @@
-local function init_part4()
-    aptools = mil
+local counter = 0
+
+-- LIBRARY - ap selector
+local ap_was_found = false
+local ap_selected_ssid = ""
+local ap_selected_pass = ""
+function ap_select(t)
+    --print("HEAP ap_select "..node.heap())
+    ap_was_found = false
+
+    if nil == t then 
+        print ("Scan returned empty list.")
+        return
+    end
+
+    local ssid = ""
+    local cfg_ssid = ""
+    local cfg_pass = ""
+    local line = ""
+    
+    for ssid in pairs(t) do
+        print ("Searching password for AP "..ssid)
+        file.open("passwd.ini", "r")
+        if nil == file.open("passwd.ini", "r") then
+            print ("PANIC: file passwd.ini missing")
+            break
+            -- fatal, tak se na vse vykaslu a koncim
+        else
+            repeat
+                line = file.readline();
+                if line ~= nil then
+                    cfg_ssid, cfg_pass = string.match(line, '([^|]+)|([^|]+)|')           
+                    --print("DEBUG: ssid"..cfg_ssid..", pass"..cfg_pass)
+                    if ssid == cfg_ssid then
+                        print ("Known ssid "..ssid..", password "..cfg_pass)
+                        ap_selected_ssid = cfg_ssid
+                        ap_selected_pass = cfg_pass
+                        ap_was_found = true
+                        break
+                    end
+                end
+            until line == nil
+            file.close()
+            if ap_was_found == true then
+                break
+            end
+        end
+    end
+
+    ssid = nil
+    line = nil
+    cfg_ssid = nil
+    cfg_pass = nil
+    line = nil
+end
+-- LIBRARY end
+
+local function check_new_ip()
+    --print("HEAP check_new_ip "..node.heap())
+
+    tmr.stop(0)
     if nil == wifi.sta.getip() then 
-        print("IP unavaiable, waiting...") 
-        tmr.alarm(0, 2000, 0, function() init_part4() end)
+        print("Waiting for IP...") 
+        counter = counter - 1
+        if (counter > 0) then
+            tmr.alarm(0, 2000, 0, function() check_new_ip() end)
+        else
+            print(wifi.sta.status())
+            print("PANIC, not IP assigned, end")
+            dofile("sleep.lc")
+        end
     else 
         print("Config done, IP is "..wifi.sta.getip())
         dofile("send2cloud.lc")
     end
 end
 
-local function init_part2()
-    print("Searching AP...")
-    aptools = require("aptools")
-    wifi.sta.getap(aptools.select_ap)
-    if aptools.found() == 0 then -- nebylo nalezeno
-        print("Waiting before AP scan...")
-        tmr.alarm(00, 3000, 0, function() init_part2() end)   
-        -- musi cekat 3 sekundy jinak to nikdy neprojde a vzdycky to neprochazi na poprve 
-        return
-    else
-        print("Connecting AP...")
+local function reset_apn()
+    --print("HEAP reset_apn "..node.heap())
+
+    tmr.stop(0)
+    print("Scanning APs...")
+    --apt = require("aptools") -- pouziva aptools, na konci zase uklidi do nil
+    counter = counter - 1
+    --wifi.sta.getap(apt.select_ap)
+    wifi.sta.getap(ap_select)
+    --if apt.found() == true then
+    if ap_was_found == true then
+        --print("Connecting to "..apt.ssid())
+        --print("Connecting to "..ap_selected_ssid)
+        --wifi.sta.config(apt.ssid(),apt.pass())
+        wifi.sta.disconnect()
+        wifi.sta.config(ap_selected_ssid,ap_selected_pass)
         wifi.sta.connect()
-        tmr.alarm(0, 5000, 0, function() init_part4() end)
+        wifi.sta.autoconnect(1)
+        counter = 10
+        tmr.alarm(0, 5000, 0, function() check_new_ip() end)
+    else 
+        print("Waiting between scans...")
+        if (counter > 0) then
+            tmr.alarm(0, 4000, 0, function() reset_apn() end)   
+            -- musi cekat 3 sekundy jinak to nikdy neprojde a vzdycky to neprochazi na poprve 
+        else
+            print("PANIC, not wifi coverage, end")
+            dofile("sleep.lc")
+        end 
+    end
+    --apt = nil
+end
+
+local function change_apn()
+    --print("HEAP change_apn "..node.heap())
+    
+    wifi.setmode(wifi.STATION) -- pro jistotu
+    counter = 10
+    reset_apn()
+end
+
+local function check_ip()
+--    print("HEAP check_ip "..node.heap())
+
+    tmr.stop(0)
+    if nil ~= wifi.sta.getip() then 
+        print("Autoconnected, IP is "..wifi.sta.getip())
+        dofile("send2cloud.lc")
+    else
+        print("Connecting...") 
+        counter = counter - 1
+        if (counter > 0) and (1 == wifi.sta.status()) then
+            tmr.alarm(0, 2000, 0, function() check_ip() end)
+        else
+            print(wifi.sta.status())
+            change_apn()
+        end
     end
 end
 
 -- start.lua
---tmr.alarm(1, 1000, 1, function() print(node.heap()) end) -- pokud se pouzije padne to na nedostatek pameti
-wifi.sta.config("nesmysl","nesmysl1") -- aby se to nikem neprihlasilo, jinak nefunguje poradne vyhedani 
-wifi.setmode(wifi.STATION)
-tmr.alarm(0, 1000, 0, function() init_part2() end)
+--print("HEAP start.lua "..node.heap())
+
+tmr.stop(1)
+tmr.stop(0)
+counter = 10
+check_ip()
