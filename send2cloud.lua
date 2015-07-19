8a1888fcc91040f837198572b9008ae69d63390b
horak <jaroslav.horak@jiffaco.cz>
2015-07-19 22:27:01 +0200

    Prekopano a konecne se to 100% pripojuje
    Puvodne jsem si myslel, ze to je watchdog.
    Potom dvojite volani funkci v ds18b20.lua
    Pak jsem podeziral prevody na desetine cislo
    Nakonec se vse vyresilo tim ze funkce send_data se vola pres casovac

--- send2cloud.lua
+++ send2cloud.lua
@@ -1,9 +1,5 @@
-local Temperature = 0
-local Humidity = 0
-local TempList = {}
-local Battery = 0
-local counter = 0
 local SentOK = 0
+local Fields
 
 --local api_key = "***REMOVED***" -- sklenik
 local api_key = "***REMOVED***" -- solarni system
@@ -16,20 +12,7 @@ local function send_data()
     -- prepare reboot if something bad, timeout 15 s
     tmr.alarm(0, 15000, 0, function() dofile("reboot.lc") end)
 
-    -- prepocet pole teplot na URL retezec
-    collectgarbage()
-    local Fields = ""
-    for q,v in pairs(TempList) do
-        Fields = Fields.."&field"..q.."="..v
-        TempList[q]=nil -- mazu po sobe prvky pole
-    end
-    TempList = nil -- zrusim praznde pole
-
     -- pridam velikost heapu
-    --Fields = Fields.."&field7="..Battery
-    Fields = Fields.."&field5="..math.random(100)
-    
-    -- pridam napeti baterie
     Fields = Fields.."&field8="..node.heap()
     
     print(Fields) -- debug
@@ -52,18 +35,16 @@ local function send_data()
     conn:on("disconnection", function(conn) 
         print("Got disconnection.") 
         conn = nil
-        if (SentOK == 1) then
-            SentOK = nil
+--        if (SentOK == 1) then
             dofile("wait.lc")
-        else
-            dofile("reboot.lc")
-        end
+--        else
+--            dofile("reboot.lc")
+--        end
     end)
     
     conn:on("connection", function(conn)
         SentOK = 1
         print("Connected, sending data...")
---        conn:send("GET /update?key="..api_key.."&field1="..Temperature.."&field2="..Humidity.."&field3="..Battery.." HTTP/1.1\r\n") 
         conn:send("GET /update?api_key="..api_key..Fields.." HTTP/1.1\r\n") 
         conn:send("Host: api.thingspeak.com\r\n") 
         conn:send("Accept: */*\r\n") 
@@ -83,29 +64,6 @@ local function measure_data()
     gpio.write(6, gpio.HIGH) -- zapnuti napajeni
     print("Measuring...")
     
-    -- Temperature and humidity DHT2
---    require("dht22")
---    if 0 == dht22.read(2) then -- pin 2=GPIO5 
---        dht22 = nil
---        counter = counter - 1
---        if (counter > 0) then
---            tmr.alarm(0, 500, 0, function() measure_data() end)
---        else
---            print("PANIC, data not aquired, end")
---            dofile("reboot.lc")
---        end
---        return
---    end
---    
---    Temperature = dht22.getTemperatureString()
---    Humidity = dht22.getHumidityString()
---    
---    dht22 = nil
---    package.loaded["dht22"]=nil
---    
---    print ("Temperature: "..Temperature)
---    print ("Humidity: "..Humidity)
-
     -- Tepolot z ds18b20
     t = require("ds18b20")
 
@@ -117,22 +75,27 @@ local function measure_data()
 
         -- Start measure for all sensors
         for q,v in pairs(addrs) do
-            t.measure(v)
+            t.startMeasure(v)
         end
         -- Wait until first measure is done
-        tmr.wdclr()
+--        tmr.wdclr()
         tmr.delay(750000)
-        tmr.wdclr()
+--        tmr.wdclr()
         -- Read temperatures
-        local value = 0
-        local textvalue = ""
+        local value 
+        local textvalue
+        Fields = ""
         for q,v in pairs(addrs) do
-            value = t.read(v)
+            value = nil
+            value = t.readNumber(v)
+            textvalue = nil
             textvalue = (value / 10000).."."..string.sub(string.format("%04d",(value % 10000)),1,4)
-            TempList[q] = textvalue
             print("Temperature "..q.." = "..textvalue)
             addrs[q] = nil -- mazu z pole adresu, uz ji nebudu potrebovat
+            Fields = Fields.."&field"..q.."="..textvalue
+            collectgarbage()
         end
+--        Fields = Fields.."&field1=1.0001&field2=2.0002&field3=3.0003&field4=4.0004&field5=5.0005"
     end
     addrs = nil -- rusim pole adres
     textvalue = nil 
@@ -143,22 +106,21 @@ local function measure_data()
     ds18b20 = nil
     package.loaded["ds18b20"]=nil
 
-    gpio.write(6, gpio.LOW) 
+    -- vypnuti napajeni teplomeru
+    gpio.write(6, gpio.LOW) -- tento pin slouzi k napajeni 
     gpio.write(7, gpio.LOW) -- vypnuti i datoveho pinu, zustava jinak svitit modra led na kitu, na finalu to zbytecne zere, funce ow.depower nefunguje
 
     -- Battery
-    Battery = (468 * adc.read(0)) / 100
-    print ("Battery: "..Battery)
+    -- Battery = (468 * adc.read(0)) / 100
 
 end
 
 --print("HEAP send2cloud.lua "..node.heap())
 tmr.stop(0)
---counter = 5 -- pouziva se pouze s dht, pro ds18b20 neni napsana podpora opakovani mereni
 measure_data()
 collectgarbage()
 -- nevolam ze send data, protoze se nic neopakuje a tak je lepsi aby lokalni promenne 
 -- send data zhynuli, realne to ma efekt asi 300 bajtu na heapu, takze nic moc
-send_data() 
-collectgarbage()
+  tmr.alarm(0, 500, 0, function() send_data() end)
+--send_data() 
 
