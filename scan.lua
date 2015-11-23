77d775dfa555ad0e750ba218d9561caae2fbd831
horak <jaroslav.horak@jiffaco.cz>
2015-11-23 23:40:56 +0100

    Pokus o skenovanani ve smycce, taky vyhazuje blaboly
    Mam dojem, ze te uloze se nedostava cas a obcas ji nekdo prerusi. Nebo je ten
    vstupni signal jako prase...

--- scan.lua
+++ scan.lua
@@ -10,6 +10,68 @@ cntdel = 0
 up,down = 0,0
 data_out = ""
 
+-- sw version
+local inputpin = Dout
+function pin_change3(level_)
+    gpio.mode(inputpin,gpio.INPUT,gpio.FLOAT)
+    local halfbitcount = 0
+    local lastedge = tmr.now()
+    local lastlevel = 0
+    local databits = ""
+    local level,interval,timenow
+    -- debug
+        --times = {}
+    repeat 
+        level = gpio.read(inputpin)
+        timenow = tmr.now()
+        interval = timenow - lastedge
+        if level ~= lastlevel then
+            if interval < 2000 then 
+                halfbitcount = halfbitcount + 1
+            else
+                halfbitcount = halfbitcount + 2
+            end
+            if (halfbitcount % 2) == 1 then
+                databits = databits .. level
+            end
+            lastlevel = level
+            lastedge = timenow
+
+            -- debug
+                table.insert(times,interval)
+        
+        end
+    until interval > 100000
+
+    -- debug
+        table.insert(times,interval)
+        table.insert(times,"end")
+        if halfbitcount > 10 then
+            data_out = databits
+        end
+
+    gpio.mode(inputpin,gpio.INT)
+    --gpio.trig(inputpin,"down",pin_change3)
+end
+
+
+function pin_change2(level_)
+    local del_ = tmr.now() - change
+    change = tmr.now()
+
+    if (del_ < timing) then
+        timing = timing - 1
+    end
+    if (timing*2 < del_) then
+        timing = timing + 1
+    end
+    
+    if (cntdel < 40) then
+        times[cntdel] = del_
+        cntdel = cntdel + 1
+    end
+end
+
 
 function pin_change(level_)
     local del_ = tmr.now() - change
@@ -91,19 +153,20 @@ end
 
 function print_time()
     print(timing)
-    print("^"..up.." v"..down)
+    --print("^"..up.." v"..down)
     local value,q,v="T:"
     for q,v in pairs(times) do
         value = value .. v .. " "
     end
     times = {}
-    cntdel = 0
+    --cntdel = 0
     print(value)
     print(data_out)
     print(".")
     up,down = 0,0
 end    
 
+print("jedu")
 gpio.mode(Din,gpio.INT)
-gpio.trig(Din,"both",pin_change)
+gpio.trig(Din,"down",pin_change3)
 tmr.alarm(0, 2000, 1, function() print_time() end)
