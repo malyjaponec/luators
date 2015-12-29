cef1e49330758242ade28c3925e73829e128a033
Jaroslav Horak <jaroslav.horak@jiffaco.cz>
2015-12-30 00:26:03 +0100

    Opraveno, takze to uz realne pocita

--- measure.lua
+++ measure.lua
@@ -1,14 +1,21 @@
 -- measure.lua
     tmr.stop(TM["m"])
 
+-- globalni citace pro 2 elektromer
+    PCount1 = 0
+    PCount2 = 0
+
 -- Citaci funkce
     function CitacPulzu1(level)
         PCount1 = PCount1 + 1
-        
+        --gpio.trig(Pulzy1, "down") 
+        --if level == 1 then gpio.trig(Pulzy1, "down") else gpio.trig(Pulzy1, "up") end
     end
       
     function CitacPulzu2(level)
         PCount2 = PCount2 + 1
+        --gpio.trig(Pulzy2, "down") 
+        --if level == 1 then gpio.trig(Pulzy2, "down") else gpio.trig(Pulzy2, "up") end
     end
 
 -- Odesilaci funkce
@@ -26,20 +33,16 @@
         tmr.alarm(TM["m"], 10000, 0,  function() AktivujOdeslani() end) 
     end
       
--- globalni citace pro 2 elektromer
-    PCount1 = 0
-    PCount2 = 0
-
 -- Konfigurace, kam jsou elektromery pripojeny
     Pulzy1 = GP[4]
     Pulzy2 = GP[5]
     
 -- Nastaveni pinu
     gpio.mode(Pulzy1, gpio.INT, gpioPULLUP) 
-    gpio.trig(Pulzy1, "down", CitacPulzu1())
+    gpio.trig(Pulzy1, "down", CitacPulzu1)
     if Pulzy2 ~= nil then
         gpio.mode(Pulzy2, gpio.INT, gpioPULLUP) 
-        gpio.trig(Pulzy2, "down", CitacPulzu2())
+        gpio.trig(Pulzy2, "down", CitacPulzu2)
     end
 
 -- Nacasu prvni odeslani
