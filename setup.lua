1fa5b54f1336289999459422818875a62cb2deeb
Jaroslav Horak <jaroslav.horak@jiffaco.cz>
2017-01-01 23:16:30 +0100

    Konfigurace pro ovladani ventilatoru u kamen v garazi

--- setup.lua
+++ setup.lua
@@ -5,6 +5,9 @@
     --gpionum = {[0]=3,[1]=10,[2]=4,[3]=9,[4]=1,[5]=2,[10]=12,[12]=6,[13]=7,[14]=5,[15]=8,[16]=0}
     -- uspora pameti,nevyuzivane piny nejsou v definici
     gpionum = {[0]=3,[2]=4,[4]=1,[5]=2,[12]=6,[13]=7,[14]=5,[15]=8,[16]=0}
+	
+	-- verze software
+	SW_VERSION = "3"	
 
     -- prevede ID luatoru do 36-kove soustavy, tak aby to bylo reprezentovano co nejmene znaky
     local function IDIn36(IN)
@@ -43,18 +46,18 @@
 
         -- Spustim procesy nastavujici sit, nastavi se casovac a indikacni led
         network = require("network")
-        -- network.setup(1, gpionum[13]) -- s ledkovym vystupem
-		network.setup(1, nil) -- bez ovladani ledky, muze byt vhodne pro exoticke systemy pouzivajici SPI a I2C co nemaji dost volnych pinu jeste na prdle blikani
+        network.setup(1, gpionum[15]) -- s ledkovym vystupem
+		--network.setup(1, nil) -- bez ovladani ledky, muze byt vhodne pro exoticke systemy pouzivajici SPI a I2C co nemaji dost volnych pinu jeste na prdle blikani
 
         -- Spustim proces merici baterii, ktery bezi dokud nedojde k okamizku odeslani
-        battery = require("battery")
-        battery.setup(2, nil) -- bez mereni svetla
+        --battery = require("battery")
+        --battery.setup(2, nil) -- bez mereni svetla
         --battery.setup(2,gpionum[14]) -- s merenim svetla - pouziva pouze foliovnik, mereni svetla neni presne a navic tam je proudovy unik
 
         -- Spustim proces merici senzoru
-        dht22 = require("dht22")
+        --dht22 = require("dht22")
         --dht22.setup(3,gpionum[5],nil,3) -- luatori s trvale napajenym DHT
-        dht22.setup(3,gpionum[5],gpionum[14],3) -- DHT odpojovane - napajeni z pinu
+        --dht22.setup(3,gpionum[5],gpionum[14],3) -- DHT odpojovane - napajeni z pinu
         --[[ k tomu jen to ze s novym sw je problem napajeni z pinu, protoze dht pak nemeri
              behem vysilani wifi dokud nedostane luator IP, zrejme predchozi software stihl nejake
              jedno mereni pred vysilanim a to mu stacilo, nova implementace potrebuje opakovani
@@ -66,14 +69,14 @@
 			 software pak 30s zkousi se s nima domluvit a nic nezmeni a vybiji baterky, takze je to
 			 hodne individualni jak to zapojit, zda se ze to zavisi od kusu dht
              ]]--
-        --dalas = require("dalas")
-        --dalas.setup(5,gpionum[5])
+        dalas = require("dalas")
+        dalas.setup(5,gpionum[14])
         --baro = require("baro")
         --baro.setup(4,gpionum[14],gpionum[12]) 
         --dist = require("distance")
         --dist.setup(3,50) 
-        --analog = require("analog")
-        --analog.setup(2,25)
+        analog = require("analog")
+        analog.setup(2,25)
 		--digital = require("digital")
 		--digital.capture(gpionum[4],gpionum[5])
         -- *************************
@@ -82,23 +85,24 @@
 -- *************************
 -- konstanty pro reportovani
 -- *************************
-    ReportInterval = 10*60
+    ReportInterval = 5
     --ReportIntervalFast = 1*60 -- rychlost rychlych reportu, pokud je null tak se to nepouziva
-    --PeriodicReport = 0 -- pokud je null pak se reportuje 1x a usne se, jakakoliv hodnota zpusobi neusnuti a restart po zadane dobe
+    PeriodicReport = 0 -- pokud je null pak se reportuje 1x a usne se, jakakoliv hodnota zpusobi neusnuti a restart po zadane dobe
     ReportFast = 0 -- defaultne vypnute
-    ReportNode = "3" 
+    ReportNode = "5" 
 	--[[ moje rozdeleni nodu emonu jak je pouzivam ja
 	1 plynomer, kotel a vytapeni
 	2 solarni ohrev vody
 	3 pomalu aktualizovane merici systemy z baterii (teplomery, delkomery, barometry)
 	4 elektromery
 	5 rychle merici systemy z AC (udirna) a kontrolni systemy (to co ma vystupni agenty)
-	6 testing
+	6 node red - vypoctena data ktera tlaci na emon node red systemy
+	7 testing
 	]]
 -- **********************************
 -- konstanty pro cteni dat ze serveru
 -- **********************************
-	--GetFeeds = {[642]=gpionum[4],[640]=gpionum[15]}
+	GetFeeds = {[639]=gpionum[4]}
 	
 -- ***
     ReportFieldPrefix = IDIn36(node.chipid()).."_" -- co nejkratsi jednoznacna ID luatoru z jeho SN
@@ -111,8 +115,8 @@
         file.close()
 		
 -- Prenastaveni pinu, ktere nechci pouzivat, nekdy se rozsveci RGB ledka a podobne takze zde je prostor to rucne napsat
-		gpio.mode(gpionum[15], gpio.OUTPUT) 
-		gpio.write(gpionum[15], gpio.LOW)
+	gpio.mode(gpionum[13], gpio.OUTPUT) 
+	gpio.write(gpionum[13], gpio.LOW)
 
 -- Debug, pokud existuje soubor, knihovny vypisuji veci informace se zrovna deje
         if (file.open("debug.ini", "r") ~= nil) then Debug = 1 file.close() else Debug = 0 end
@@ -121,7 +125,7 @@
         MeasureInit()
 
 -- Spustim odesilac, bez casovace primo
-        LedSend = nil --gpionum[12] -- zaporna hodnota se pouzije pokud chceme ledku spinat otevrenym kolektorem, kladna hodnota kdyz je ledka zapojena proti zemi
+        LedSend = gpionum[12] -- zaporna hodnota se pouzije pokud chceme ledku spinat otevrenym kolektorem, kladna hodnota kdyz je ledka zapojena proti zemi
         dofile("send.lc") -- pouziva casovac 0
     
 -- Uklid
